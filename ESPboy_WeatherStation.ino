3654af5fc7c65b579f8afdc5a2c3d472a27957db
ESPboy <ESPboy.edu@gmail.com>
2021-06-06 14:28:46 +0300

    updated

---
+++ ESPboy_WeatherStation.ino
@@ -0,0 +1,197 @@
+/*
+ESPboy Weather station by RomanS
+
+ESPboy project page:
+www.ESPboy.com
+https://hackaday.io/project/164830-espboy-beyond-the-games-platform-with-wifi
+*/
+
+ADC_MODE(ADC_VCC);
+
+#include "lib/ESPboyInit.h"
+#include "lib/ESPboyInit.cpp"
+#include <BMx280I2C.h>
+#include "RTClib.h"
+
+ESPboyInit myESPboy;
+BMx280I2C bmx280(0x76);
+RTC_DS3231 rtc;
+ 
+const char *daysOfTheWeek[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+const char *months[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sen", "Oct", "Nov", "Dec"};
+static DateTime now;
+static float hum;  //relative humidity [%]
+static float ahum; //absolute humidity [%]
+static float temp; //temperature [C]
+static double pres; //atm.pressure [Pa]
+
+
+void runButtonsCommand(uint8_t bt){
+  myESPboy.myLED.setRGB(0,0,20);
+  myESPboy.playTone (800, 20);
+
+  if (bt&PAD_LEFT) {
+      if (now.day() < 31) rtc.adjust (DateTime(now.year(), now.month(), (now.day()+1), now.hour(), now.minute(), now.second()));
+      else rtc.adjust (DateTime(now.year(), now.month(), 1, now.hour(), now.minute(), now.second()));
+      now = rtc.now();
+  }
+  if (bt&PAD_RIGHT) {
+       if (now.month() < 12) rtc.adjust (DateTime(now.year(), now.month()+1, now.day(), now.hour(), now.minute(), now.second()));
+       else rtc.adjust (DateTime(now.year(), 1, now.day(), now.hour(), now.minute(), now.second()));
+       now = rtc.now();
+  }
+  if (bt&PAD_UP){
+       rtc.adjust (DateTime(now.year()+1, now.month(), now.day(), now.hour(), now.minute(), now.second()));
+       now = rtc.now();
+  }
+  if (bt&PAD_DOWN){
+       rtc.adjust (DateTime(now.year()-1, now.month(), now.day(), now.hour(), now.minute(), now.second()));
+       now = rtc.now();
+  }
+  if (bt&PAD_ACT){
+       if (now.hour() < 24) rtc.adjust (DateTime(now.year(), now.month(), now.day(), now.hour()+1, now.minute(), now.second()));
+       else rtc.adjust (DateTime(now.year(), now.month(), now.day(), 0, now.minute(), now.second()));
+       now = rtc.now();
+  }
+  if (bt&PAD_ESC){
+       if (now.minute() < 60) rtc.adjust (DateTime(now.year(), now.month(), now.day(), now.hour(), now.minute()+1, 0));
+       else rtc.adjust (DateTime(now.year(), now.month(), now.day(), now.hour(), 0, 0));
+       now = rtc.now();
+   }
+}
+  
+
+void batVoltageDraw(){
+  uint8_t vcc;
+  vcc = map(ESP.getVcc(), 3500, 4200, 0, 100);
+  myESPboy.tft.setTextSize(1);
+  myESPboy.tft.setTextColor(TFT_WHITE);
+  myESPboy.tft.setCursor(110, 120);
+  myESPboy.tft.print(vcc);
+  myESPboy.tft.print("%");
+}
+
+void drawled(){
+  int8_t temp = bmx280.getTemperature();
+  myESPboy.myLED.setRGB(0,10,0);
+  if (temp < 0) myESPboy.myLED.setRGB(0,0,10);
+  if (temp > 35) myESPboy.myLED.setRGB(10,0,0);
+}
+
+ 
+ 
+void printtft(){
+ 
+//draw date
+  myESPboy.tft.setTextColor(TFT_YELLOW);
+  myESPboy.tft.setTextSize(1);
+  myESPboy.tft.setCursor (20, 0);
+  if (now.day() < 10) myESPboy.tft.print ("0");
+  myESPboy.tft.print (now.day());
+  myESPboy.tft.print (" ");
+  myESPboy.tft.print (months[now.month() - 1]);
+  myESPboy.tft.print (" ");
+  myESPboy.tft.print (now.year());
+  myESPboy.tft.print (" ");
+  myESPboy.tft.print (daysOfTheWeek[now.dayOfTheWeek()]);
+ 
+//draw time
+  myESPboy.tft.setTextColor(TFT_WHITE);
+  myESPboy.tft.setTextSize(3);
+  myESPboy.tft.setCursor (21, 18);
+  if (now.hour() < 10) myESPboy.tft.print ("0");
+  myESPboy.tft.print (now.hour());
+  myESPboy.tft.print (":");
+  if (now.minute() < 10) myESPboy.tft.print ("0");
+  myESPboy.tft.print (now.minute());
+ 
+//draw temp
+  myESPboy.tft.setTextColor(TFT_GREEN);
+  myESPboy.tft.setCursor (0, 49);
+  myESPboy.tft.setTextSize(1);
+  myESPboy.tft.print ("Temp   ");
+  myESPboy.tft.setTextSize(2);
+  myESPboy.tft.print ((int16_t)round(temp));
+  myESPboy.tft.setTextSize(1);
+  myESPboy.tft.print ("C");
+ 
+//draw humidity
+  myESPboy.tft.setTextColor(TFT_GREEN);
+  myESPboy.tft.setCursor (0, 69);
+  myESPboy.tft.setTextSize(1);
+  myESPboy.tft.print ("Humid  ");
+  myESPboy.tft.setTextSize(2);
+  myESPboy.tft.print ((int16_t)round(hum));
+  myESPboy.tft.setTextSize(1);
+  myESPboy.tft.print ("% ");
+  myESPboy.tft.setTextSize(2);
+  myESPboy.tft.print ((int16_t)round(ahum));
+  myESPboy.tft.setTextSize(1);
+  myESPboy.tft.print ("g/m3");
+ 
+//draw pressure
+  myESPboy.tft.setTextColor(TFT_GREEN);
+  myESPboy.tft.setCursor (0, 89);
+  myESPboy.tft.setTextSize(1);
+  myESPboy.tft.print ("Atm.pr ");
+  myESPboy.tft.setTextSize(2);
+  myESPboy.tft.print ((int16_t)round(pres));
+  myESPboy.tft.setTextSize(1);
+  myESPboy.tft.print ("mmHg");
+
+  batVoltageDraw();
+}
+ 
+ 
+void setup() {
+//Init ESPboy
+  myESPboy.begin("Weather Station");
+ 
+//BME280 init
+  myESPboy.tft.setTextColor(TFT_RED);
+  myESPboy.tft.setTextSize(1);
+  myESPboy.tft.setCursor(0,120);
+  if (!bmx280.begin()){
+    myESPboy.tft.print (F("Weather module FAILED"));
+    while (1) delay(100);}
+  bmx280.resetToDefaults();
+  bmx280.writeOversamplingPressure(BMx280MI::OSRS_P_x16);
+  bmx280.writeOversamplingTemperature(BMx280MI::OSRS_T_x16);
+  bmx280.writeOversamplingHumidity(BMx280MI::OSRS_H_x16);
+  bmx280.measure();
+  while (!bmx280.hasValue()) delay(100);
+ 
+//RTC init
+  if (!rtc.begin()) {
+    myESPboy.tft.print (F("Weather module FAILED"));
+    while (1) delay(100);}
+
+}
+ 
+ 
+void loop() {
+ static long count; 
+ if (millis() > count + 3000){
+     count = millis();
+     while (!bmx280.hasValue()) delay(100);
+     now = rtc.now();
+     bmx280.measure();
+     temp = bmx280.getTemperature();
+     hum = bmx280.getHumidity();
+     if (hum >99) hum=99;
+     ahum = 216.7f * ((hum / 100.0f) * 6.112f * exp((17.62f * temp) / (243.12f + temp)) / (273.15f + temp));
+     if (ahum >99) ahum=99;
+     pres = bmx280.getPressure() / 133.3d;
+     myESPboy.tft.fillScreen(TFT_BLACK);
+     printtft();
+     drawled();
+ }
+  uint8_t bt = myESPboy.getKeys();
+  if (bt) { 
+    runButtonsCommand(bt); 
+    myESPboy.tft.fillScreen(TFT_BLACK); 
+    printtft();
+    drawled();
+    delay(200);
+  }
+}
